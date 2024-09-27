4a62a9c87a9abec9f518a063602219cb0eadff52
amirh <amirh9476@gmail.com>
2024-09-27 19:43:18 +0330

    add Dockerfile docker-compose.yml to project

--- core/asgi.py
+++ core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for did_pardaz_saba project.
+ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'did_pardaz_saba.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
